4ae204cd767a5014b58a03afd634d525ef49132b
Yuzhu Xu <s2232496@bioinfmsc5.bio.ed.ac.uk>
2021-10-24 00:31:32 +0100

    First step and try

---
+++ Assignment1.sh
@@ -0,0 +1,6 @@
+#step1: check the quality
+fastqc /localdisk/data.local/BPSM/Assignment1/fastq/*.fq.gz -o /localdisk/home/s2232496/AY21A1/step1/
+
+#get the output of step1
+#step2: assess the numbers and quality of the output of step1
+ 
